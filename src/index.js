611d4f50e82bc172c3cc18688f173e52513a0473
Leonardo Oliveira <c.p.leonardooliveira@gmail.com>
2021-07-06 19:08:49 -0300

    Created the account creation feature in the application

--- src/index.js
+++ src/index.js
@@ -1,5 +1,32 @@
+const { response } = require('express');
 const express = require('express')
+const { v4 } = require('uuid')
 
 const app = express();
+
+app.use(express.json())
+
+const customers = [];
+
+/**
+ * cpf - string
+ * name - string,
+ * id - uuid
+ * statment - []
+ */
+
+app.post("/account", (req, res) => {
+  const { name, cpf  } = req.body;
+
+  customers.push({
+    name,
+    cpf,
+    id: v4(),
+    statement: []
+  })
+
+
+  return res.status(201).send();
+});
 
 app.listen(3333)
